c75a83762f7d518a6369abd76f3db42078afe61f
LiNk-NY <marcel.ramos@roswellpark.org>
2024-05-08 17:18:37 -0400

    add build function for tools::pkgs2HTML

---
+++ R/build_html_mans.R
@@ -0,0 +1,26 @@
+
+#' @examples
+#'
+#' bioc_sub <- c(
+#'     "SummarizedExperiment", "Biobase", "BiocBaseUtils",
+#'     "BiocGenerics", "DelayedArray", "GenomicRanges",
+#'     "IRanges", "S4Vectors"
+#' )
+#'
+#' packages <- file.path(normalizePath("~/bioc"), bioc_sub)
+#' src_base <- "~/minibioc/packages/3.20/bioc"
+#'
+#' build_html_mans(packages, src_base)
+#'
+#' @export
+build_html_mans <- function(packages_dir, src_base) {
+    packages <- basename(package_dir)
+    html_dir <- file.path(base_dir, "manuals")
+    if (!dir.exists(html_dir)) dir.create(html_dir, recursive = TRUE)
+    outfiles <- file.path(html_dir, paste0(packages, ".html"))
+    Map(
+        tools::pkg2HTML,
+        dir = packages_dir,
+        out = outfiles
+    )
+}
